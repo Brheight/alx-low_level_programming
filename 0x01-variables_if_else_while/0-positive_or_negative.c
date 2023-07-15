4c1395bd63813a7dda77d08a4f41d97170887b3f
Bright Adewole <brightoadewole@gmail.com>
2023-07-14 22:06:28 -0700

    c  betty  ds;cha

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry point, assigns a random number to n and prints if it's positive,
- *        negative, or zero.
- * Return: Always 0 (Success)
+ * main - Entry point, assigns a random number to n 
+ * * Return: Always 0 (Success)
  */
 int main(void)
 {
