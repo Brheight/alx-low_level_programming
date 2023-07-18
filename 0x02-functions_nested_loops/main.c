ec1d52d97b4241ad50a6046e352d1a62bc5ff888
Bright Adewole <brightoadewole@gmail.com>
2023-07-18 13:27:00 -0700

    swp jkj

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
+void print_alphabet(void);
+
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
     print_alphabet();
     return 0;
 }
+
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
+}
+
+/* Your _putchar implementation goes here */
