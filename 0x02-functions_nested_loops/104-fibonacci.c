7e4cafb855f0e65d1f8051a36637fcc2a81a7790
Bright Adewole <brightoadewole@gmail.com>
2023-07-19 16:05:26 -0700

    ttuujj

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <gmp.h>
 
 /**
  * main - Entry point
@@ -8,22 +9,31 @@
 int main(void)
 {
     int count, maxCount;
-    unsigned long fib1 = 1, fib2 = 2, nextFib;
+    mpz_t fib1, fib2, nextFib;
 
     maxCount = 98;
 
-    printf("%lu, %lu", fib1, fib2);
+    mpz_init_set_ui(fib1, 1);
+    mpz_init_set_ui(fib2, 2);
+
+    printf("%s, %s", mpz_get_str(NULL, 10, fib1), mpz_get_str(NULL, 10, fib2));
 
     for (count = 3; count <= maxCount; count++)
     {
-        nextFib = fib1 + fib2;
-        printf(", %lu", nextFib);
+        mpz_init(nextFib);
+        mpz_add(nextFib, fib1, fib2);
+        printf(", %s", mpz_get_str(NULL, 10, nextFib));
 
-        fib1 = fib2;
-        fib2 = nextFib;
+        mpz_set(fib1, fib2);
+        mpz_set(fib2, nextFib);
+
+        mpz_clear(nextFib);
     }
 
     printf("\n");
+
+    mpz_clear(fib1);
+    mpz_clear(fib2);
 
     return 0;
 }
