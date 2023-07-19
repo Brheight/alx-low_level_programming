fc51623d618ca1dd26923bd2a4c16259fe12c167
Bright Adewole <brightoadewole@gmail.com>
2023-07-19 15:37:36 -0700

    ttuujj

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,9 +7,21 @@
  */
 int main(void)
 {
+    int fib1 = 1;
+    int fib2 = 2;
+    int count = 0;
+    char tots[1000];
+    sprintf(tots, "%d, %d", fib1, fib2);
 
+    while (count < 96) {
+        int newfib = fib2 + fib1;
+        count++;
+        sprintf(tots + strlen(tots), ", %d", newfib);
+        fib1 = fib2;
+        fib2 = newfib;
+    }
 
-    printf("\n");
+    printf("%s\n", tots);
 
     return 0;
 }
