26c2553c3f2e6bcecb398f63240531087bc600b9
Bright Adewole <brightoadewole@gmail.com>
2023-07-19 05:33:11 -0700

    4 alpha 45

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,12 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+int sum_of_multiples(void);
+void print_fibonacci(void);
+void print_sum_of_even_fibonacci(void);
 
 #endif /* MAIN_H */
