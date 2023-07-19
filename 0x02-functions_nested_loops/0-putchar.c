84f35aa75c5a585794da4c0c00ff758054bd5e2c
Bright Adewole <brightoadewole@gmail.com>
2023-07-18 21:23:49 -0700

    puch 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,34 +3,20 @@
 /**
  * main - Entry point
  *
- * Description: Prints _putchar, followed by a new line.
+ * Description: Prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char *str = "_putchar\n";
-    int i = 0;
+	char str[] = "_putchar\n";
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    return 0;
-}
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- *
- * @c: The character to be written
- *
- * Return: On success 1
- * On error, -1 is returned and errno is set appropriately
- */
-int _putchar(char c)
-{
-    /* Your code here */
-    return (c == '\n' ? write(1, &c, 1) : write(1, &c, 1));
+	return (0);
 }
