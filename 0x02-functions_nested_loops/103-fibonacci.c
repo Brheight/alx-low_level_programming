134fa4809156cf1df8c8790baea55267f31d23c1
Bright Adewole <brightoadewole@gmail.com>
2023-07-19 13:55:42 -0700

    swp

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    long int num1 = 1, num2 = 2, sum = 0, fib;
+	long int num1 = 1, num2 = 2, sum = 0, fib;
 
-    while (num1 <= 4000000)
-    {
-        if (num1 % 2 == 0)
-            sum += num1;
+	while (num1 <= 4000000)
+	{
+		if (num1 % 2 == 0)
+			sum += num1;
 
-        fib = num1 + num2;
-        num1 = num2;
-        num2 = fib;
-    }
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+	}
 
-    printf("%ld\n", sum);
+	printf("%ld\n", sum);
 
-    return 0;
+	return (0);
 }
