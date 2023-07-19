45262d6993d4148e0c7451a5b0e59e3201c8b3bd
Bright Adewole <brightoadewole@gmail.com>
2023-07-19 14:14:00 -0700

    nwq

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,21 @@
  */
 int main(void)
 {
-    int count, maxCount;
-    unsigned long fib1 = 1, fib2 = 2, nextFib;
+	int i;
+	unsigned long fib1 = 1, fib2 = 2, fib;
 
-    maxCount = 98;
+	printf("%lu, %lu", fib1, fib2);
 
-    printf("%lu, %lu", fib1, fib2);
+	for (i = 3; i <= 98; i++)
+	{
+		fib = fib1 + fib2;
+		printf(", %lu", fib);
 
-    for (count = 3; count <= maxCount; count++)
-    {
-        nextFib = fib1 + fib2;
-        printf(", %lu", nextFib);
+		fib1 = fib2;
+		fib2 = fib;
+	}
 
-        fib1 = fib2;
-        fib2 = nextFib;
-    }
+	printf("\n");
 
-    printf("\n");
-
-    return 0;
+	return 0;
 }
