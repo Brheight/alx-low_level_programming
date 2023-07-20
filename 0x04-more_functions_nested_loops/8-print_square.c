f4e00ab49e0513db20bcd1347a287ec2ca444669
Bright Adewole <brightoadewole@gmail.com>
2023-07-20 13:37:26 -0700

    aey gj7

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square in the terminal.
- * @size: The size of the square.
+ * print_square - Prints a square using the character '#'
+ * @size: The size of the square
+ *
+ * Return: void
  */
 void print_square(int size)
 {
+    int i, j;
+
     if (size <= 0)
     {
         _putchar('\n');
         return;
     }
 
-    int i, j;
     for (i = 0; i < size; i++)
     {
         for (j = 0; j < size; j++)
-        {
             _putchar('#');
-        }
         _putchar('\n');
     }
 }
