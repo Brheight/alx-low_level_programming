48997a14f686d226958b5cbe7ae6a071670e24d9
Bright Adewole <brightoadewole@gmail.com>
2023-07-24 15:08:13 -0700

    sstr

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    dest[i] = '\0'; /* Add the terminating null byte */
+	dest[i] = '\0'; /* Add the terminating null byte */
 
-    return dest;
+	return dest;
 }
