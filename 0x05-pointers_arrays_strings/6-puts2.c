7b6cf67440cb21719c45aaa90fc5dbc5def6b128
Bright Adewole <brightoadewole@gmail.com>
2023-07-24 15:48:31 -0700

    sstr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
-	_putchar('\n');
+    while (str[i] != '\0' && str[i + 1] != '\0')
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
 }
