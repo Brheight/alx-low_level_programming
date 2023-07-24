2b46ff30b2103f2a8daa6b357284f426e4ad12cf
Bright Adewole <brightoadewole@gmail.com>
2023-07-24 14:46:17 -0700

    sstr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string
+ * puts2 - Prints every other character of a string, starting with the first
  * @str: Pointer to the string
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i += 2;
-    }
-
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
 }
