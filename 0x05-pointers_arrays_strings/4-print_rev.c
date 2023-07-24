7e19dbf105531674dcd64a570b078e8bcda24100
Bright Adewole <brightoadewole@gmail.com>
2023-07-24 13:31:44 -0700

    trrd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: The length of the string (number of characters)
+ */
+int _strlen(char *s)
+{
+    int length = 0;
+
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+
+    return length;
+}
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to the string
+ */
+void print_rev(char *s)
+{
+    int len = _strlen(s);
+
+    for (int i = len - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: The length of the string (number of characters)
+ */
+int _strlen(char *s)
+{
+    int length = 0;
+
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+
+    return length;
+}
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to the string
+ */
+void rev_string(char *s)
+{
+    int len = _strlen(s);
+    int start = 0;
+    int end = len - 1;
+
+    while (start < end)
+    {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+}
