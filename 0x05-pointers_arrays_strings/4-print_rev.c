66ab26ff97f16b20ff6e0cd7ff280672a567ecdd
Bright Adewole <brightoadewole@gmail.com>
2023-07-24 14:20:53 -0700

    sstr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
 
-    return length;
+	return (length);
 }
 
 /**
@@ -25,12 +25,12 @@ int _strlen(char *s)
  */
 void print_rev(char *s)
 {
-    int len = _strlen(s);
+	int len = _strlen(s);
 
-    int i;
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	int i;
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
