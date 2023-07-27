28c162c92b40ca9ced5c105a843287941d7cb6d5
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 15:47:41 -0700

    rtt

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ char *rot13(char *str);
 void print_number(int n);
 char *_strncpy(char *dest, char *src, int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
