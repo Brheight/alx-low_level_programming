7df7410e994bff53da779abd604099cfd6ee35d0
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 18:06:00 -0700

    ugj

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,12 +7,36 @@
  */
 int main(void)
 {
-    int a[5] = {98, 198, 298, 398, 498};
-    int *p;
+	int a[] = {0, 1, 2, 3, 4, 98, 6, 7, 8, 9};
+	int *p;
 
-    p = a + 2;
-    printf("a[2] = %d\n", *(p));
-    *(a + 2) = 98;
-    printf("a[2] = %d\n", *(p));
-    return (0);
+	p = &a[2];
+	*(p + 5) = 98;
+	print_array(a, 10);
+	return (0);
+}
+
+/**
+ * print_array - Prints an array of integers
+ * @a: The array to print
+ * @n: Number of elements in the array
+ *
+ * Return: Nothing (void)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(a[i] + '0');
+		i++;
+	}
+	_putchar('\n');
 }
