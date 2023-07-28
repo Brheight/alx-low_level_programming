ae52f53d058e1a80fcf53d065ca33067664f9017
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 17:48:06 -0700

    ugj

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,22 @@
  */
 char *leet(char *str)
 {
-    char *ptr = str;
-    char *leet_chars = "aAeEoOtTlL";
-    char *leet_codes = "4433007711";
-    int i, j;
+	char *ptr = str;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_codes = "4433007711";
+	int i, j;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; leet_chars[j] != '\0'; j++)
-        {
-            if (str[i] == leet_chars[j])
-            {
-                str[i] = leet_codes[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_chars[j] != '\0'; j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_codes[j];
+				break;
+			}
+		}
+	}
 
-    return ptr;
+	return (ptr);
 }
