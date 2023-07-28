a62e733a8b047612a4f13c1f5720f93ba049756c
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 17:34:24 -0700

    ugj

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,16 +8,16 @@
  */
 char *string_toupper(char *str)
 {
-    char *ptr = str;
+	char *ptr = str;
 
-    while (*ptr != '\0')
-    {
-        if (*ptr >= 'a' && *ptr <= 'z')
-        {
-            *ptr = *ptr - ('a' - 'A');
-        }
-        ptr++;
-    }
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
 
-    return str;
+	return (str);
 }
