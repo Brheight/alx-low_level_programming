4bafc3812d2d6c1e0e05ebcee2049d2c48a3abdd
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 17:27:15 -0700

    ugj

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 != '\0' && *s2 != '\0')
-    {
-        if (*s1 != *s2)
-        {
-            return (*s1 - *s2);
-        }
-        s1++;
-        s2++;
-    }
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 
-    /* If we reach here, both strings are equal until null terminator */
-    return 0;
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
 }
