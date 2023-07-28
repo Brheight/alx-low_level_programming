61e0bf56c24147333154293dd478c1224bb753bf
Bright Adewole <brightoadewole@gmail.com>
2023-07-27 17:16:41 -0700

    ugj

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,24 +10,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0;
-    int i = 0;
+	int dest_len = 0;
+	int i = 0;
 
-    /* Find the length of the destination string */
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
 
-    /* Append characters from src to dest up to n bytes */
-    while (src[i] != '\0' && i < n)
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
+	/* Append characters from src to dest up to n bytes */
+	while (src[i] != '\0' && i < n)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
 
-    /* Add null terminator to the end of dest */
-    dest[dest_len + i] = '\0';
+	/* Add null terminator to the end of dest */
+	dest[dest_len + i] = '\0';
 
-    return dest;
+	return dest;
 }
