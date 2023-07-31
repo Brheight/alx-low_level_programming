5e18fa2eb40a578b729ad04188ec09bb0a44b87e
Bright Adewole <brightoadewole@gmail.com>
2023-07-31 15:39:34 -0700

    array string

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    if (t)
+        printf("%s\n", t);
+    else
+        printf("No matching character found.\n");
+
+    return (0);
+}
