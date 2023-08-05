c0d28b951ae38aa87c2ff76a4ac4886b35a2651a
Bright Adewole <brightoadewole@gmail.com>
2023-08-04 18:30:22 -0700

    added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,34 +3,32 @@
 
 int is_positive_number(char *str)
 {
-    int i = 0;
-    char c;
+	int i = 0;
+	char c;
 
-    while ((c = str[i]))
-    {
-        if (c < '0' || c > '9')
-            return 0;
-        i++;
-    }
-    return 1;
+	while ((c = str[i]))
+	{
+		if (c < '0' || c > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int main(int argc, char *argv[])
 {
-    int i, sum = 0;
+	int i, sum = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        if (is_positive_number(argv[i]))
-            sum += atoi(argv[i]);
-        else
-        {
-            printf("Error\n");
-            return 1;
-        }
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
