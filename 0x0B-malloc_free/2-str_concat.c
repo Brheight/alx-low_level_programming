748d3a0756939f503dadef34efe26cdaf2a6e7f4
Bright Adewole <brightoadewole@gmail.com>
2023-08-09 16:04:26 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,32 +8,24 @@
  * @s1: The first string.
  * @s2: The second string.
  *
- * Return: A pointer to a newly allocated space in memory containing the
- *         concatenated string, or NULL on failure.
+ * Return: A pointer to a newly allocated space
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concatenated;
-    size_t len1 = 0, len2 = 0, i, j;
+	char *concatenated;
+	size_t len1 = 0, len2 = 0, i, j;
 
-    if (s1 != NULL)
-        len1 = strlen(s1);
-
-    if (s2 != NULL)
-        len2 = strlen(s2);
-
-    concatenated = malloc(len1 + len2 + 1);
-
-    if (concatenated == NULL)
-        return NULL;
-
-    for (i = 0; i < len1; i++)
-        concatenated[i] = s1[i];
-
-    for (j = 0; j < len2; j++)
-        concatenated[i + j] = s2[j];
-
-    concatenated[i + j] = '\0';
-
-    return concatenated;
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
+	concatenated = malloc(len1 + len2 + 1);
+	if (concatenated == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		concatenated[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		concatenated[i + j] = s2[j];
+	concatenated[i + j] = '\0';
+	return (oncatenated)
 }
