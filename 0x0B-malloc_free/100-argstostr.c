23cfec9aa3b5b6c9d5d4391af65f04fb7cdc1c53
Bright Adewole <brightoadewole@gmail.com>
2023-08-09 17:17:29 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,50 +3,45 @@
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all the arguments of a program.
+ * argstostr - Concatenates all the arguments
  * @ac: The number of arguments.
  * @av: An array of pointers to the arguments.
  *
- * Return: A pointer to a new string containing concatenated arguments,
- *         or NULL if ac == 0 or av == NULL or if it fails.
+ * Return: A pointer to a new string
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, len = 0, total_len = 0;
-    char *concatenated;
+	int i, j, len = 0, total_len = 0;
+	char *concatenated;
 
-    if (ac == 0 || av == NULL)
-        return NULL;
-
-    for (i = 0; i < ac; i++)
-    {
-        len = 0;
-        while (av[i][len] != '\0')
-        {
-            len++;
-            total_len++;
-        }
-        total_len++; // To account for the newline character
-    }
-
-    concatenated = malloc(sizeof(char) * total_len + 1);
-    if (concatenated == NULL)
-        return NULL;
-
-    total_len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        len = 0;
-        while (av[i][len] != '\0')
-        {
-            concatenated[total_len] = av[i][len];
-            len++;
-            total_len++;
-        }
-        concatenated[total_len] = '\n';
-        total_len++;
-    }
-    concatenated[total_len] = '\0';
-
-    return concatenated;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		while (av[i][len] != '\0')
+		{
+			len++;
+			total_len++;
+		}
+		total_len++;
+	}
+	concatenated = malloc(sizeof(char) * total_len + 1);
+	if (concatenated == NULL)
+		return (NULL);
+	total_len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		while (av[i][len] != '\0')
+		{
+			concatenated[total_len] = av[i][len];
+			len++;
+			total_len++;
+		}
+		concatenated[total_len] = '\n';
+		total_len++;
+	}
+	concatenated[total_len] = '\0';
+	return (concatenated);
 }
