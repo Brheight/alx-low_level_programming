8348f2ee92fe691b522e5f433fc4a82463859b4d
Bright Adewole <brightoadewole@gmail.com>
2023-08-11 16:50:49 -0700

    malloc free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,30 +15,27 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
     void *new_ptr;
 
-    if (new_size == old_size)
-        return ptr;
-
-    if (ptr == NULL)
-        return malloc(new_size);
-
-    if (new_size == 0)
+    if (new_size == 0 && ptr != NULL)
     {
         free(ptr);
         return NULL;
     }
+
+    if (new_size == old_size)
+        return ptr;
 
     new_ptr = malloc(new_size);
 
     if (new_ptr == NULL)
         return NULL;
 
-    if (new_size > old_size)
-        new_size = old_size;
+    if (ptr != NULL)
+    {
+        for (unsigned int i = 0; i < old_size && i < new_size; i++)
+            *((char *)new_ptr + i) = *((char *)ptr + i);
 
-    for (unsigned int i = 0; i < new_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
-
-    free(ptr);
+        free(ptr);
+    }
 
     return new_ptr;
 }
