7e37e7981e90b9c3c9e1b9977629c1b614f04fcf
Bright Adewole <brightoadewole@gmail.com>
2023-08-11 17:49:33 -0700

    malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,21 +3,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    if (nmemb == 0 || size == 0) {
-        return NULL; 
-    }
+    if (nmemb == 0 || size == 0)
+        return NULL;
 
-    void *ptr = malloc(nmemb * size); 
+    void *ptr = malloc(nmemb * size);
 
-    if (ptr == NULL) {
-        return NULL; 
-    }
+    if (ptr == NULL)
+        return NULL;
 
 
-    unsigned char *byte_ptr = ptr;
-    for (unsigned int i = 0; i < nmemb * size; i++) {
-        byte_ptr[i] = 0;
+    for (unsigned int i = 0; i < nmemb * size; i++)
+    {
+        *((char *)ptr + i) = 0;
     }
 
     return ptr;
 }
+
