a712876d66a2151b9a673b5c8047c905b57fa539
Bright Adewole <brightoadewole@gmail.com>
2023-08-11 17:32:31 -0700

    malloc free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,8 +12,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_positive_number(char *str);
 int _strlen(char *str);
-void *_calloc(unsigned int nmemb, unsigned int size);
 void perform_multiplication(char *num1, char *num2, int len1, int len2, int *result);
-void _putchar(char c);
 
 #endif /* MAIN_H */
