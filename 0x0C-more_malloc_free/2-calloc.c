7069e4080951ed636f59436b07ce965c5231a762
Bright Adewole <brightoadewole@gmail.com>
2023-08-11 17:56:27 -0700

    malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,29 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdlib.h> // Include the necessary header for malloc and free
 
 /**
- * _isdigit - Checks if a character is a digit.
- * @c: The character to check.
+ * _calloc - Allocates memory for an array of nmemb elements of size bytes each.
+ * The memory is set to zero.
+ * @nmemb: Number of elements in the array.
+ * @size: Size of each element in bytes.
  *
- * Return: 1 if c is a digit, 0 otherwise.
+ * Return: Pointer to the allocated memory, or NULL on failure.
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+    void *ptr;
+
     if (nmemb == 0 || size == 0)
         return NULL;
 
-    void *ptr = malloc(nmemb * size);
+    ptr = malloc(nmemb * size);
+
 
     if (ptr == NULL)
         return NULL;
 
+
     for (unsigned int i = 0; i < nmemb * size; i++)
-    {
         *((char *)ptr + i) = 0;
-    }
 
     return ptr;
 }
-
