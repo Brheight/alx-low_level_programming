2a59daecd5f66608b0e71f539fc2b1cca19a6768
Bright Adewole <brightoadewole@gmail.com>
2023-08-15 16:32:05 -0700

    stryctyre

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,18 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes a struct dog
- * @d: Pointer to the struct dog variable to initialize
+ * init_dog - Initializes struct dog
+ * @d: Pointer to the struct
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != NULL)
-    {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
-    }
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
