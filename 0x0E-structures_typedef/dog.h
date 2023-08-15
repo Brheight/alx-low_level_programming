ea7144588dc3a738511fede2c8dec189e4494040
Bright Adewole <brightoadewole@gmail.com>
2023-08-15 16:48:08 -0700

    stryctyre

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - Defines a dog's information
+ * struct dog - Defines a dog info
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
  */
 struct dog {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-/* Define the new type dog_t */
 typedef struct dog dog_t;
 
-/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
