7aad31923b908326a7fec257acc48398301895de
Bright Adewole <brightoadewole@gmail.com>
2023-08-16 16:53:03 -0700

    function s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stddef.h>
+#include <stddef.h>
 
 /**
  * array_iterator - applies a given function to each element of an array
@@ -10,9 +12,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+    size_t i;    
     if (array && action)
     {
-        for (size_t i = 0; i < size; i++)
+        for (i = 0; i < size; i++) 
         {
             action(array[i]);
         }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 
@@ -7,5 +8,6 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
