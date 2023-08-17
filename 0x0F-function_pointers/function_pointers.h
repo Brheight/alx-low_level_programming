ed1bbd8dba200c2db14f879c0ae5a8aef7092be6
Bright Adewole <brightoadewole@gmail.com>
2023-08-16 17:01:22 -0700

    functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,6 @@ void print_name_uppercase(char *name);
 
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
