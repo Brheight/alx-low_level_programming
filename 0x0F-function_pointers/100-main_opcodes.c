1dd9014497e55bafeb3adb24615142b3c5c797c0
Bright Adewole <brightoadewole@gmail.com>
2023-08-16 17:38:42 -0700

    functions

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,35 +6,31 @@
  * @argc: Number of arguments
  * @argv: Array of arguments
  *
- * Return: 0 on success, 1 if incorrect arguments, 2 if negative number of bytes
+ * Return: 0 on success, 1 if incorrect argument
  */
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    char *main_opcode;
+	int num_bytes, i;
+	char *main_opcode;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    num_bytes = atoi(argv[1]);
-
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
-
-    main_opcode = (char *)&main;
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02hhx", main_opcode[i]);
-        if (i != num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
-
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	main_opcode = (char *)&main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx", main_opcode[i]);
+		if (i != num_bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
