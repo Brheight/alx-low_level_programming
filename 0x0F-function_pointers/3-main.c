f43083e7abac17cbb944f32a77cd15e4c0047a00
Bright Adewole <brightoadewole@gmail.com>
2023-08-16 18:02:00 -0700

    functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,21 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*func)(int, int);
+	int num1, num2, result;
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        fprintf(stderr, "Error\n");
-        return (98);
-    }
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    func = get_op_func(argv[2]);
-
-    result = func(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+	if (argc != 4)
+	{
+		fprintf(stderr, "Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	result = func(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
