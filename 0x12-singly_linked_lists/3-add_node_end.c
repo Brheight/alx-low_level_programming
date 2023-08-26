e9dcf0efddf047482ae93ee5c905719db3217ed5
Bright Adewole <brightoadewole@gmail.com>
2023-08-25 18:41:13 -0700

    print list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,36 +11,29 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
+	list_t *new_node, *temp;
 
-    if (!str)
-        return NULL;
-
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return NULL;
-
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return NULL;
-    }
-
-    new_node->len = strlen(str);
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return new_node;
-    }
-
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = new_node;
-
-    return new_node;
+	if (!str)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
