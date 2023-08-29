23fb14b1a38eaed99ba39c418f176557a96ee220
Bright Adewole <brightoadewole@gmail.com>
2023-08-29 12:35:39 -0700

    103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,35 +1,32 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the starting node of a loop in a linked list.
- * @head: Pointer to the head node of the linked list.
- * Return: Address of the node where the loop starts, or NULL if no loop.
+ * find_listint_loop - Finds the starting node.
+ * @head: Pointer to the head node of the list.
+ * Return: Address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-    if (head == NULL || head->next == NULL)
-        return (NULL);
-
-    tortoise = head->next;
-    hare = head->next->next;
-
-    while (hare && hare->next)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
-            while (tortoise != hare)
-            {
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-            return (tortoise);
-        }
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-    }
-
-    return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	tortoise = head->next;
+	hare = head->next->next;
+	while (hare && hare->next)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return (NULL);
 }
