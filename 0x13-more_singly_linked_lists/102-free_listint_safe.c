348d9f6b74a1af2e6fa3e6c5c897f4687636c525
Bright Adewole <brightoadewole@gmail.com>
2023-08-29 12:40:34 -0700

    103

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,28 +9,25 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *current, *temp;
-    size_t count = 0;
+	listint_t *current, *temp;
+	size_t count = 0;
 
-    if (h == NULL || *h == NULL)
-        return (0);
-
-    current = *h;
-    while (current)
-    {
-        temp = current;
-        current = current->next;
-        temp->next = NULL;
-        free(temp);
-        count++;
-
-        if (current == *h)
-        {
-            *h = NULL;
-            break;
-        }
-    }
-
-    *h = NULL;
-    return (count);
+	if (h == NULL || *h == NULL)
+		return (0);
+	current = *h;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		temp->next = NULL;
+		free(temp);
+		count++;
+		if (current == *h)
+		{
+			*h = NULL;
+			break;
+		}
+	}
+	*h = NULL;
+	return (count);
 }
