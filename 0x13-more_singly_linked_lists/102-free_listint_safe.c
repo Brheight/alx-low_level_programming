48a040b8a7fb66c81a4b8d22dc4e9dcc7f561cbc
Bright Adewole <brightoadewole@gmail.com>
2023-08-29 12:03:48 -0700

    list hash

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: A pointer to a pointer to the head of the list
+ *
+ * Return: The size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+    listint_t *current, *temp;
+    size_t count = 0;
+
+    if (h == NULL || *h == NULL)
+        return (0);
+
+    current = *h;
+    while (current)
+    {
+        temp = current;
+        current = current->next;
+        temp->next = NULL;
+        free(temp);
+        count++;
+
+        if (current == *h)
+        {
+            *h = NULL;
+            break;
+        }
+    }
+
+    *h = NULL;
+    return (count);
+}
